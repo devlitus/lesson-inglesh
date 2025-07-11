a3d863ccbfa3268ac4ddf30e4e8d4060711e0ccf
devlitus <developercarles@gmail.com>
2025-07-11 16:22:56 +0200

    feat: wrap App with DesignSystemProvider
    Add DesignSystemProvider to enable design system context for the application

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { DesignSystemProvider } from './design-system/index.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <DesignSystemProvider >
+      <App />
+    </DesignSystemProvider>
   </StrictMode>,
 )
